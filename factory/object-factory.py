9692f6b240037c1c93c82b02bc3738469c14a25b
becca4011 <becca4011@naver.com>
2021-01-11 21:31:44 +0900

    [test] faker

---
+++ factory/object-factory.py
@@ -0,0 +1,7 @@
+from faker import Faker
+
+if __name__ == "__main__":
+    f = Faker('ko_KR')
+
+    for i in range(10):
+        print(f.name())
--- readme.md
+++ readme.md
@@ -6,3 +6,8 @@
 ## 2. enter virtualenv
 
 <code>. venv/scripts/activate</code>
+
+## About Project
+
+- 파이썬으로 객체 직렬화, 저장, 불러오기
+- faker 모듈 사용
